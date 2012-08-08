4175bcc4464b8e65a1e3eb4718cd4c917acb1239
David Lawrence Miller <dave@ninepointeightone.net>
2012-08-08 18:56:20 -0400

    first set of report plots

---
+++ report-Sep2012/makeplots.R
@@ -0,0 +1,119 @@
+# make the plots for the report
+
+load("../uri-lt-data.RData")
+
+library(ggplot2)
+
+### preamble
+
+# general stuff that gets used by everthing goes here
+
+# plotting options
+p.opts <- opts(panel.grid.major=theme_blank(), 
+               panel.grid.minor=theme_blank(), 
+               panel.background=theme_rect(),
+               strip.background=theme_blank(),
+               legend.key=theme_blank(),
+               aspect.ratio=1
+              )
+
+# build a coastline object
+coast.geom <- geom_path(aes(x=x,y=y),data=coast) 
+
+# x and y limits 
+xlims <- scale_x_continuous(limits=c(-40,40))
+ylims <- scale_y_continuous(limits=c(-40,40)) 
+
+
+
+
+### all seasons by species group
+p.points <- ggplot(obs) + geom_point(aes(x=x,y=y,size=size),colour=I("blue"))
+p <- p.points + facet_wrap(~Group,nrow=5)
+p <- p + coast.geom
+p <- p + p.opts + xlims + ylims + xlab("") + ylab("")
+
+print(p)
+
+ggsave(file="all-seasons-by-group.pdf",width=15,height=20)
+
+
+# clean up
+rm(p)
+gc()
+
+
+### all species by season
+p <- p.points + facet_wrap(~Season,nrow=2)
+p <- p + coast.geom
+p <- p + p.opts + xlims + ylims + xlab("") + ylab("")
+
+#print(p)
+
+ggsave(file="all-groups-by-season.pdf",width=15,height=20)
+
+
+
+### groups by season
+p <- p.points + facet_grid(Group~Season)
+p <- p + coast.geom
+p <- p + p.opts + xlims + ylims + xlab("") + ylab("")
+
+# this takes AAAAAAAAAGES
+#print(p)
+
+ggsave(file="groups-by-season.pdf",width=15,height=40,plot=p)
+
+
+
+#### Non-geographical plots
+# plotting options
+p.opts <- opts(panel.grid.major=theme_blank(), 
+               panel.grid.minor=theme_blank(), 
+               panel.background=theme_rect(),
+               strip.background=theme_blank(),
+               legend.key=theme_blank()
+              )
+
+### histogram of distances per group
+levels(obs$Bin)[4] <- "Not\nrecorded" # so that it fits
+p <- ggplot(obs)
+p <- p + geom_histogram(aes(x=Bin))
+p <- p + facet_wrap(~Group, nrow=5, scales="free_y")
+p <- p + p.opts + xlab("bin") + ylab("Number of groups")
+
+print(p)
+ggsave(file="groups-dist-hist.pdf",width=15,height=20)
+
+
+### boxplots of size per group -- all seasons
+p <- ggplot(obs)
+p <- p + geom_boxplot(aes(x=Group,y=size))
+p <- p + p.opts + xlab("Species group") + ylab("Observed group size")
+
+print(p)
+ggsave(file="groups-size-boxplot.pdf",width=15,height=8)
+
+
+### boxplots of size per group -- per season
+p <- ggplot(obs)
+p <- p + geom_boxplot(aes(x=Group,y=size))
+p <- p + facet_wrap(~Season,ncol=1,scales="free_y")
+p <- p + p.opts + xlab("Species group") + ylab("Observed group size")
+
+print(p)
+ggsave(file="groups-size-per-season-boxplot.pdf",width=15,height=20)
+
+
+### histogram of size per group -- per season
+#p <- ggplot(obs)
+#p <- p + geom_histogram(aes(x=size))
+#p <- p + facet_grid(Group~Season,scales="free_x")
+#p <- p + p.opts + xlab("Frequency") + xlab("Observed group size")
+#
+#print(p)
+#ggsave(file="groups-size-per-season-hist.pdf",width=15,height=20)
+
+
+
+
